4bfebf63cf55b90f2173fc0ab5b92b3f29dd5543
BanLowi <lowi40k@gmail.com>
2025-12-19 09:44:09 +0100

    refactor: rename project to sst-react and clean up dependencies
    style: standardize CSS formatting and improve readability
    
    refactor: simplify main rendering logic and remove unused imports
    
    fix: enhance search functionality and improve date formatting in HomePage
    
    feat: add navigation link and format date display in TravelPage

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,8 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import "bootstrap/dist/js/bootstrap.bundle.min.js";
-import 'bootstrap-icons/font/bootstrap-icons.css';
-
+import * as bootstrap from 'bootstrap'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <App />
 )
